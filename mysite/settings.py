01e61693edc0b800b6a2d441f1dc3109d6a8b337
tomaslm <tomaslm@hotmail.com>
2020-05-04 16:44:24 -0300

    Inclusion of channel router

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # my apps
     "chat",
+    "channels",
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+
+ASGI_APPLICATION = "mysite.routing.application"
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("chat", include("chat.urls")),
+    path("chat/", include("chat.urls")),
 ]
