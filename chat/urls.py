adf4595a817046f483f4be8cb856c2ab2efb1e83
tomaslm <tomaslm@hotmail.com>
2020-05-04 17:30:49 -0300

    Inclusion of test view, to send messages to rooms via http request

--- chat/urls.py
+++ chat/urls.py
@@ -4,5 +4,10 @@ from chat import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:room_name>/", views.room, name="room"),
+    path("room/<str:room_name>/", views.room, name="room"),
+    path(
+        "admin_message/<str:room_name>/<str:message>/",
+        views.send_admin_message_to_room,
+        name="admin_message",
+    ),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,6 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -7,3 +10,11 @@ def index(request):
 
 def room(request, room_name):
     return render(request, "chat/room.html", {"room_name": room_name})
+
+
+def send_admin_message_to_room(request, room_name, message):
+    layer = get_channel_layer()
+    async_to_sync(layer.group_send)(
+        f"chat_{room_name}", {"type": "chat_message", "message": message}
+    )
+    return HttpResponse(f"Message {message} sent to {room_name}")
